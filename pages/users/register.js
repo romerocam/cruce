dc929de4449b30c3b96267bfbfde38724fb50242
stani <sberberova@gmail.com>
2022-09-09 15:09:17 -0400

    pages de login y register

--- pages/users/register.js
+++ pages/users/register.js
@@ -1,10 +1,10 @@
 import { Flex, Input, Button, Avatar } from "@chakra-ui/react";
 import Link from "next/link";
 
-const UsersPage = () => {
+const Register = () => {
   return (
     <Flex height="100vh" alignItems="center" justifyContent="center" direction='column'>
-       <Avatar bg='teal.500' mb={10}/>
+       <Avatar bg='teal.500' mb={10} />
       <Flex
         width="70vw"
         direction="column"
@@ -14,28 +14,35 @@ const UsersPage = () => {
         rounded={6}
       >
         <Flex alignItems="center" justifyContent="center" mb={6}>
-          <span className="bold-underlined" color="00A8E8">
-            Log in
-          </span>
-          <Link href="/users/register">
-            <Button>Register</Button>
+          <Link href="/users">
+            <Button>Log in</Button>
           </Link>
+          <span className="bold-underlined">Register</span>
         </Flex>
-        <Flex direction="column" alignItems="center" justifyContent="center">
+        <Flex direction="column" justifyContent="center">
+          <Input type="text" placeholder="Name" variant="flushed" mb={3} />
+          <Input type="text" placeholder="Last Name" variant="flushed" mb={3} />
+          <Input type="text" placeholder="DNI" variant="flushed" mb={3} />
           <Input type="email" placeholder="E-mail" variant="flushed" mb={3} />
           <Input
             type="password"
             placeholder="Password"
             variant="flushed"
+            mb={3}
+          />
+          <Input
+            type="password"
+            placeholder="Confirm Password"
+            variant="flushed"
             mb={6}
           />
         </Flex>
         <Button colorScheme="teal" variant="solid">
-          Log in
+          Submit
         </Button>
       </Flex>
     </Flex>
   );
 };
 
-export default UsersPage;
+export default Register;
