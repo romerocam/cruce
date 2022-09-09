cafc43449c9612d67439b23ab7a5a08209cf49c8
romerocam <romerocam@hotmail.com>
2022-09-09 11:56:02 -0300

    first commit

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 import mongoose from "mongoose";
+const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
 
 const UserSchema = new mongoose.Schema({
   name: String,
@@ -7,21 +10,47 @@ const UserSchema = new mongoose.Schema({
   address: String,
   password: {
     type: String,
-    required: [true, "password required"],
+    required: true,
+    // minlength: 7,
   },
   email: {
     type: String,
-  },
+    required: [true, "email required"],
+    unique: true,
+    lowerCase: true,
+    validate: [isEmail, "enter valid email"],
+},
   roles: {
     type: String,
-    enum: {
-      values: ['user', 'admin', 'operator'],
-      message: '{VALUE} is not supported'
-    }
-  } ,  
+    default: 'guest',
+    enum: ['guest', 'admin', 'operator'],
+  },  
   operator: String,
+  // tokens: [
+  //   {
+  //     token: {
+  //       type: String,
+  //       required: true,
+  //     },
+  //   },
+  // ],
 },
 { versionKey: false }
 );
+// UserSchema.methods.generateAuthToken = async function() {
+//   const user = this;
+//   const token = jwt.sign({ _id: user._id.toString() }, 'mySecret');
+//   user.tokens = user.tokens.concat({ token });
+//   await user.save();
+//   return token;
+// };
+
+// UserSchema.pre('save', async function(next) {
+//   const user = this;
+//   if (user.isModified('password')) {
+//     user.password = await bcrypt.hash(user.password, 8);
+//   }
+//   next();
+// });
 
 module.exports = mongoose.models.User || mongoose.model("User", UserSchema);
