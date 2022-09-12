df3fb2aa9ec19b121887d14855b24de5cf5d10a0
NataliaBebebino <nbebebino@gmail.com>
2022-09-12 11:38:09 -0400

    Change status code in Office route

--- pages/api/office/index.js
+++ pages/api/office/index.js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
     case "POST":
       try {
         const office = await Office.create(req.body);
-        res.status(200).json({
+        res.status(201).json({
           success: true,
           data: office,
         });
