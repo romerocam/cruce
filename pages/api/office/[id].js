4bfacbabff97842cfb4a221111e2ea4eeff07f73
NataliaBebebino <nbebebino@gmail.com>
2022-09-12 10:24:39 -0400

    Create dynamic Office route

--- pages/api/office/[id].js
+++ pages/api/office/[id].js
@@ -3,28 +3,14 @@ import Office from "../../../models/Office";
 
 export default async function handler(req, res) {
   const { method } = req;
+  const officeId = req.query.id;
 
   await connectMongo();
 
   switch (method) {
     case "GET":
       try {
-        const offices = await Office.find({});
-        res.status(200).json({
-          success: true,
-          data: offices,
-        });
-      } catch (error) {
-        res.status(400).json({
-          success: false,
-          error: error.message,
-        });
-      }
-      break;
-
-    case "POST":
-      try {
-        const office = await Office.create(req.body);
+        const office = await Office.findOne({_id: officeId});
         res.status(200).json({
           success: true,
           data: office,
