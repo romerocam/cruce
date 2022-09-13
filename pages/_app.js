559462551db729a00d605a0f0376715a02e60142
Cris-Alvarez09 <c.alvarez9612@gmail.com>
2022-09-13 18:55:04 -0500

    Creacion del perfil del usuario y header

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,24 @@
 import '../styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
+
+const colors = {
+  brand: {
+    900: '#FFFFFB',
+    800: '#000505',
+    700: '#00A8E8',
+    600: '#0987A0'
+  },
+}
+
+
+const theme = extendTheme({ colors })
 
 function MyApp({ Component, pageProps }) {
 
 
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   )
