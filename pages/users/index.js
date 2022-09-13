652d16fc0e576b07a76624bc1a4ac3ce725c8c70
romerocam <romerocam@hotmail.com>
2022-09-13 15:57:29 -0300

    commiting jwt

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,8 +1,30 @@
 import Link from "next/link";
-import { Flex, Input, Button, Avatar , Stack} from "@chakra-ui/react";
-
+import { Flex, Input, Button, Avatar, Stack } from "@chakra-ui/react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
 
 const UsersPage = () => {
+  const router = useRouter();
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await axios.post("/api/auth/login", credentials);
+    console.log("response", response);
+    if (response.status === 200) {
+      router.push("/dashboard");
+    }
+  };
   return (
     <Flex
       height="100vh"
@@ -28,23 +50,30 @@ const UsersPage = () => {
           </Link>
         </Flex>
 
-        <form>
-          <Stack spacing={3}> 
-            <Input type="email" placeholder="E-mail" variant="flushed"/>
+        <form onSubmit={handleSubmit}>
+          <Stack spacing={3}>
+            <Input
+              type="email"
+              placeholder="E-mail"
+              variant="flushed"
+              name="email"
+              onChange={handleChange}
+            />
             <Input
               type="password"
               placeholder="Password"
               variant="flushed"
+              name="password"
+              onChange={handleChange}
             />
             <Link href="/users/forgotpassword">
               <span>Forgot your password?</span>
             </Link>
-            <Button type='submit' colorScheme="teal" variant="solid">
+            <Button type="submit" colorScheme="teal" variant="solid">
               Log in
             </Button>
           </Stack>
         </form>
-   
       </Flex>
     </Flex>
   );
