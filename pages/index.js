fd310120628cbb6de0707c6c479be5a9f02a7861
Cris-Alvarez09 <c.alvarez9612@gmail.com>
2022-09-14 11:14:05 -0500

    Merge dev into local profileUserCA

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,42 @@
+//next.js
+import { useRouter } from "next/router";
+//components
+import Navbar from "./components/Navbar";
+//styles
+import { Flex, Box, Button } from "@chakra-ui/react";
+
 const HomePage = () => {
-  return <hi>Cruce Booking App</hi>;
+  const router = useRouter();
+  return (
+    <>
+      <Navbar />
+      <Flex
+        direction="column"
+        align="center"
+        justify="center"
+        height="80"
+        width="100"
+        margin="20px"
+        background="teal.400"
+      >
+        <Box color="white" fontSize="50px" fontWeight="bold">
+          Appointment Booking
+        </Box>
+        <Box color="white" fontSize="30px" fontWeight="bold">
+          Made Easy!!!
+        </Box>
+        <Button
+          mt="80px"
+          onClick={() => {
+            router.push("/users/register");
+          }}
+        >
+          Get Started
+        </Button>
+      </Flex>
+    </>
+  );
+
 };
 
 export default HomePage;
--- pages/users/register.js
+++ pages/users/register.js
@@ -2,8 +2,9 @@ import axios from "axios";
 //react
 import { useForm } from "react-hook-form";
 //Next.js
-import Link from "next/link";
-
+import { useRouter } from "next/router";
+//components
+import Navbar from "../components/Navbar";
 //styles
 import {
   Flex,
@@ -15,125 +16,160 @@ import {
   FormControl,
 } from "@chakra-ui/react";
 
+
 const Register = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm({ mode: "onBlur" });
+
+  const router = useRouter()
 
   const onSubmit = (formData) => {
     console.log("esta es la form data", formData);
     axios
       .post("/api/users", formData)
-      .then((response) => response.data)
+      .then((response) => {
+        router.push("/users")
+        return response.data
+      })
       .catch((error) => console.log(error));
   };
 
   return (
-    <Flex
-      height="100vh"
-      alignItems="center"
-      justifyContent="center"
-      direction="column"
-    >
-      <Avatar bg="teal.500" mb={10} />
+    <div>
+      <Navbar />
       <Flex
-        width="90vw"
-        direction="column"
+        height="100vh"
         alignItems="center"
-        background="gray.100"
-        p={2}
-        rounded={6}
+        justifyContent="flex-start"
+        direction="column"
       >
-        <Flex alignItems="center" justifyContent="center" mb={6}>
-          <Link href="/users">
-            <Button>Log in</Button>
-          </Link>
-          <span className="bold-underlined">Register</span>
-        </Flex>
+        <Avatar bg="teal.500" m={10} />
+        <Flex
+          width="90vw"
+          direction="column"
+          alignItems="center"
+          background="gray.100"
+          p={10}
+          rounded={6}
+        >
 
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <FormControl isInvalid={errors.name}>
-            <Stack spacing={3}>
-              <Input
-                type="text"
-                placeholder="Name"
-                variant="flushed"
-                id="name"
-                {...register("name")}
-              />
-              <Input
-                type="text"
-                placeholder="Last Name"
-                variant="flushed"
-                id="lastname"
-                {...register("lastname")}
-              />
-              <Input
-                type="text"
-                placeholder="DNI"
-                variant="flushed"
-                id="dni"
-                {...register("dni", {
-                  minLength: {
-                    value: 8,
-                    message: "Please enter 8 digits",
-                  },
-                  maxLength: {
-                    value: 8,
-                    message: "Please enter 8 digits",
-                  },
-                })}
-              />
-              <Input
-                type="text"
-                placeholder="address"
-                variant="flushed"
-                name="address"
-                {...register("address")}
-              />
-              <Input
-                type="email"
-                placeholder="E-mail"
-                variant="flushed"
-                id="email"
-                {...register("email", {
-                  required: "E-mail is required",
-                })}
-              />
-              <Input
-                type="password"
-                placeholder="Password"
-                variant="flushed"
-                id="password"
-                {...register("password", {
-                  required: "Password is required",
-                  minLength: {
-                    value: 7,
-                    message: "Minimum length is 7",
-                  },
-                })}
-              />
-              <Input
-                type="password"
-                placeholder="Confirm Password"
-                variant="flushed"
-                id="confirmpassword"
-              />
-              <Button
-                type="submit"
-                colorScheme="teal"
-                variant="solid"
-                margin={6}
-              >
-                Submit
-              </Button>
-            </Stack>
-          </FormControl>
-        </form>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <FormControl isInvalid={errors.name || errors.lastname || errors.dni || errors.address || errors.email || errors.password || errors.confirmpassword}>
+              <Stack spacing={3}>
+                <Input
+                  type="text"
+                  placeholder="Name"
+                  variant="flushed"
+                  focusBorderColor="teal.400"
+                  errorBorderColor="none"
+                  id="name"
+                  {...register("name", {
+                    required: "Name is required"
+                  })}
+                />
+                <FormErrorMessage>{errors.name && errors.name.message}</FormErrorMessage>
+                <Input
+                  type="text"
+                  placeholder="Last Name"
+                  variant="flushed"
+                  focusBorderColor="teal.400"
+                  errorBorderColor="none"
+                  id="lastname"
+                  {...register("lastname", {
+                    required: "Last Name is required"
+                  })}
+                />
+                <FormErrorMessage>{errors.lastname && errors.lastname.message}</FormErrorMessage>
+                <Input
+                  type="text"
+                  placeholder="DNI"
+                  variant="flushed"
+                  focusBorderColor="teal.400"
+                  errorBorderColor="none"
+                  id="dni"
+                  {...register("dni", {
+                    required: "DNI is required",
+                    minLength: {
+                      value: 8,
+                      message: "Please enter 8 digits",
+                    },
+                    maxLength: {
+                      value: 8,
+                      message: "Please enter 8 digits",
+                    },
+                  })}
+                />
+                <FormErrorMessage>{errors.dni && errors.dni.message}</FormErrorMessage>
+                <Input
+                  type="text"
+                  placeholder="Address"
+                  variant="flushed"
+                  focusBorderColor="teal.400"
+                  errorBorderColor="none"
+                  id="address"
+                  {...register("address", {
+                    required: "Address is required"
+                  })}
+                />
+                <FormErrorMessage>{errors.address && errors.address.message}</FormErrorMessage>
+                <Input
+                  type="email"
+                  placeholder="E-mail"
+                  variant="flushed"
+                  focusBorderColor="teal.400"
+                  errorBorderColor="none"
+                  id="email"
+                  {...register("email", {
+                    required: "E-mail is required",
+                  })}
+                />
+                <FormErrorMessage>{errors.email && errors.email.message}</FormErrorMessage>
+                <Input
+                  type="password"
+                  placeholder="Password"
+                  variant="flushed"
+                  focusBorderColor="teal.400"
+                  errorBorderColor="none"
+                  id="password"
+                  // name="password"
+                  {...register("password", {
+                    required: "Password is required",
+                    minLength: {
+                      value: 7,
+                      message: "Minimum length is 7",
+                    },
+                  })}
+                />
+                <FormErrorMessage>{errors.password && errors.password.message}</FormErrorMessage>
+                <Input
+                  type="password"
+                  placeholder="Confirm Password"
+                  variant="flushed"
+                  id="confirmpassword"
+                  focusBorderColor="teal.400"
+                  errorBorderColor="none"
+                  {...register("confirmpassword", {
+                    validate: value => value === password.value || "The passwords do not match"
+                  })}
+                />
+                <FormErrorMessage>{errors.confirmpassword && errors.confirmpassword.message}</FormErrorMessage>
+                <Button
+                  type="submit"
+                  colorScheme="teal"
+                  variant="solid"
+                  margin={6}
+                >
+                  Submit
+                </Button>
+              </Stack>
+            </FormControl>
+          </form>
+        </Flex>
       </Flex>
-    </Flex>
+    </div>
   );
 };
 
