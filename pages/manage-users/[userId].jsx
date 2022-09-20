a1ec354e1fca0bd4707c333786d6928c0927f29a
romerocam <romerocam@hotmail.com>
2022-09-20 18:48:37 -0300

    borrando

--- pages/manage-users/[userId].jsx
+++ pages/manage-users/[userId].jsx
@@ -25,46 +25,7 @@ const EditUserPage = () => {
    
   )
 }
-// getStaticPaths
-export async function getStaticPaths() {
-  const res = await fetch('http://localhost:3000/api/users/');
-  const data = await res.json();
-  console.log('aca',data);
- // Getting the unique key of the user from the response
- // with the map method of JavaScript.
-  // const paths= data.map((user) => {
-  //   return {
-  //     params:{id:user.id.toString()}
-  //   }
-  // })
-  // return{
-  //   paths,
-  //   fallback: false
-  // }
 
-}
-export async function getStaticProps(context) {
-  // Obtain the user’s unique ID.
-  const id = context.params.id
-
-  // Append the ID as a parameter to the API endpoint.
-  const res = await fetch(`http://localhost:3000/api/users/${userId}`)
-  const data = await res.json()
-  return {
-    props: {
-      user:data,
-    },
-  }
-}
 
 export default EditUserPage
 
-// export async function getServerSideProps(context){
-//   const user = await axios.get(`http://localhost:3000/api/users/${context.params.userId}`).then(response=>response.data.data)
-//   console.log("axios user", user)
-//   return{
-//     props:{
-//       userData: user
-//     }
-//   }
-// }
