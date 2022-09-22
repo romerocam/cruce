b34aaa697c645dac44b01b277517c8605e0b12de
romerocam <romerocam@hotmail.com>
2022-09-22 15:21:39 -0300

    operator table

--- pages/users/adminpanel.jsx
+++ pages/users/adminpanel.jsx
@@ -2,6 +2,7 @@ import AdminPanel from "../../src/components/Admin/AdminPanel";
 import Layout from "../../src/components/common/Layout/Layout";
 
 import React from 'react'
+import { getSession } from "next-auth/react";
 
 export default function MyAppointments () {
   return (
@@ -10,3 +11,25 @@ export default function MyAppointments () {
     </Layout>
   )
 }
+
+// export async function getServerSideProps(context) {
+
+//   // console.log("CONTEXT", context)
+
+//   const session = await getSession({ req: context.req })
+
+//   //console.log("SESSION", session)
+
+//   if (session === null || session.user.role !== "admin") {
+//     return {
+//       redirect: {
+//         destination: '/',
+//         permanent: false,
+//       }
+//     }
+//   }
+//   return {
+//     props: { session }
+//   }
+
+// }
