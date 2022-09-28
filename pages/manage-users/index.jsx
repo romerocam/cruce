722dcb239c18a01eb3ee4d1d0344012cc37852dc
romerocam <romerocam@hotmail.com>
2022-09-28 14:09:29 -0300

    pagination OK by nati

--- pages/manage-users/index.jsx
+++ pages/manage-users/index.jsx
@@ -1,23 +1,15 @@
 import ManageUsers from "../../src/components/Admin/ManageUsers";
-//import {getAllUsers} from "../api/users/index"
-import axios from "axios";
+
 import Layout from "../../src/components/common/Layout/Layout";
-import { useEffect, useState } from "react";
+
 import { getSession } from "next-auth/react";
 
 function ManageUsersPage() {
-  const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get("http://localhost:3000/api/users")
-      .then((response) => setUsers(response.data.data))
-      .catch((error) => error.message);
-  }, []);
 
   return (
     <Layout>
-      <ManageUsers users={users} />
+      <ManageUsers />
     </Layout>
   );
 }
