3d1c5ddfd14f3e4bc8cfbf6463c84f5935bc9b75
NataliaBebebino <nbebebino@gmail.com>
2022-09-28 15:15:11 -0400

    WIP - Implement countdown timer

--- pages/users/new-booking.jsx
+++ pages/users/new-booking.jsx
@@ -1,12 +1,37 @@
 import Layout from "../../src/components/common/Layout/Layout";
 import Calendar from "../../src/components/Calendar/Calendar";
+import Countdown from "react-countdown";
+import { Box } from "@chakra-ui/react";
+
+function refreshPage() {
+  window.location.reload(false);
+}
+
 
 function NewBooking() {
-    return (
-      <Layout>
-        <Calendar />
-      </Layout>
-    );
-  }
-  
-  export default NewBooking;
+  return (
+    <Layout>
+      <Box
+        textAlign="center"
+        backgroundColor="orange"
+        width="100px"
+        margin="auto"
+        mt="5"
+        color="white"
+        fontWeight="700"
+        borderRadius="10"
+
+      >
+        <Countdown
+          date={Date.now() + 1000000}
+          onComplete={() => {
+            refreshPage();
+          }}
+        ></Countdown>
+      </Box>
+      <Calendar />
+    </Layout>
+  );
+}
+
+export default NewBooking;
