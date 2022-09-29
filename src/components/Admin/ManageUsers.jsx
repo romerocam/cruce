de58de1d0c9d2cae2a4df35f29f67eac6e7c75e4
Agrossio <abossio@gmail.com>
2022-09-29 18:15:41 -0300

    Back Buttons OK

--- src/components/Admin/ManageUsers.jsx
+++ src/components/Admin/ManageUsers.jsx
@@ -14,15 +14,18 @@ import {
   Button,
   Container,
   Stack,
+  useColorModeValue,
 } from "@chakra-ui/react";
 import axios from "axios";
 
 export default function ManageUsers() {
-  const router = useRouter();
   const [users, setUsers] = useState([]);
   const [page, setPage] = useState(1);
   const [pageCount, setPageCount] = useState(0);
   const [pagination, setPagination] = useState([]);
+
+  const router = useRouter();
+  const colorBg = useColorModeValue("brand.700", "brand.600");
 
   useEffect(() => {
     axios
@@ -49,7 +52,7 @@ export default function ManageUsers() {
   }, [users]);
   function handlePrevious() {
     setPage((p) => {
-      
+
       if (parseInt(p) === 1) return parseInt(p);
       return parseInt(p) - 1;
     });
@@ -66,65 +69,65 @@ export default function ManageUsers() {
   }
   return (
     <>
-    <Box>
-      <Box display="flex" justifyContent="center">
-        <Input
-          placeholder="Search User "
-          width="auto"
-          marginTop="5px"
-          onChange={enteredSearchHandler}
-        />
+      <Box>
+        <Box display="flex" justifyContent="center">
+          <Input
+            placeholder="Search User "
+            width="auto"
+            marginTop="5px"
+            onChange={enteredSearchHandler}
+          />
+        </Box>
+        <Box display="flex" justifyContent="center">
+          <TableContainer
+            w="60rem"
+            border="1px"
+            borderColor="gray.400"
+            rounded="md"
+            boxShadow="md"
+            m={5}
+          >
+            <Table variant="striped" colorScheme="blue" size="sm">
+              <Thead>
+                <Tr>
+                  <Th>Name</Th>
+                  <Th>Last Name</Th>
+                  <Th>DNI</Th>
+                  <Th>E-mail</Th>
+                  <Th>Role</Th>
+                  <Th>Edit User</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {users
+                  .filter((user) =>
+                    user.name.toLowerCase().includes(enteredSearchInput.toLowerCase()) || user.lastname.toLowerCase().includes(enteredSearchInput.toLowerCase()) || user.dni.toString().toLowerCase().includes(enteredSearchInput.toLowerCase()) || user.email.toLowerCase().includes(enteredSearchInput.toLowerCase()) || user.role.toLowerCase().includes(enteredSearchInput.toLowerCase()))
+                  .map((user) => (
+                    <Tr key={user._id}>
+                      <Td>{user.name}</Td>
+                      <Td>{user.lastname}</Td>
+                      <Td>{user.dni}</Td>
+                      <Td>{user.email}</Td>
+                      <Td>{user.role}</Td>
+                      <Td>
+                        <Box>
+                          <Button
+                            leftIcon={<EditIcon />}
+                            size="xs"
+                            onClick={() => {
+                              router.push(`/manage-users/${user._id}`);
+                            }}
+                          ></Button>
+                        </Box>
+                      </Td>
+                    </Tr>
+                  ))}
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </Box>
       </Box>
-      <Box display="flex" justifyContent="center">
-        <TableContainer
-          w="60rem"
-          border="1px"
-          borderColor="gray.400"
-          rounded="md"
-          boxShadow="md"
-          m={5}
-        >
-          <Table variant="striped" colorScheme="blue" size="sm">
-            <Thead>
-              <Tr>
-                <Th>Name</Th>
-                <Th>Last Name</Th>
-                <Th>DNI</Th>
-                <Th>E-mail</Th>
-                <Th>Role</Th>
-                <Th>Edit User</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {users
-                .filter((user) =>
-                user.name.toLowerCase().includes(enteredSearchInput.toLowerCase()) || user.lastname.toLowerCase().includes(enteredSearchInput.toLowerCase()) || user.dni.toString().toLowerCase().includes(enteredSearchInput.toLowerCase()) || user.email.toLowerCase().includes(enteredSearchInput.toLowerCase()) || user.role.toLowerCase().includes(enteredSearchInput.toLowerCase()))
-                .map((user) => (
-                  <Tr key={user._id}>
-                    <Td>{user.name}</Td>
-                    <Td>{user.lastname}</Td>
-                    <Td>{user.dni}</Td>
-                    <Td>{user.email}</Td>
-                    <Td>{user.role}</Td>
-                    <Td>
-                      <Box>
-                        <Button
-                          leftIcon={<EditIcon />}
-                          size="xs"
-                          onClick={() => {
-                            router.push(`/manage-users/${user._id}`);
-                          }}
-                        ></Button>
-                      </Box>
-                    </Td>
-                  </Tr>
-                ))}
-            </Tbody>
-          </Table>
-        </TableContainer>
-      </Box>
-    </Box>
-    <Container maxW="2xl" centerContent>
+      <Container maxW="2xl" centerContent>
         <Stack direction="row column" spacing={4} align="center">
           <Button
             colorScheme="teal"
@@ -134,7 +137,7 @@ export default function ManageUsers() {
           >
             Previous
           </Button>
-          {}
+          { }
           <Button
             colorScheme="teal"
             variant="solid"
@@ -155,6 +158,19 @@ export default function ManageUsers() {
                 return <option key={index}>{index + 1}</option>;
               })}
           </select>
+        </Stack>
+        <Stack direction={"row"} alignItems={"center"} paddingTop={5}>
+          <Button
+            onClick={() => router.push("/users/adminpanel")}
+            bg={colorBg}
+            color={"white"}
+            w="md"
+            _hover={{
+              bg: colorBg,
+            }}
+          >
+            Back
+          </Button>
         </Stack>
       </Container>
     </>
